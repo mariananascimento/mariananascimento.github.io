f9ff23ea643e1aa5e6df0dfd8ef388e18cd83710
mariananascimento <47419099+mariananascimento@users.noreply.github.com>
2020-05-17 02:48:33 -0300

    fix chrome rendering by using images instead of box-shadows
    also, adds entrance effects

--- script.js
+++ script.js
@@ -1,12 +1,61 @@
-let isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
+const app = {
 
-if ( isChrome ) {
-  
-  let el = document.querySelector(':root');
-  let radius = getComputedStyle(el).getPropertyValue('--radius');
-  
-  if ( parseInt( radius ) > 133 ) {
-    el.style.setProperty('--radius', '133px');
+  element : document.body,
+
+  effects : {
+
+    apply : function( effect, delay ) {
+
+      delay = delay || 250;
+
+      const delayLoop = (fn, delay) => {
+        return (x, i) => {
+          setTimeout(() => {
+            fn(x);
+          }, i * delay);
+        };
+      };
+
+      const elements = document.querySelectorAll( '[data-effect="' + effect + '"]' );
+
+      const apply = el => el.dataset.effect = 'done';
+
+      elements.forEach( delayLoop( apply, delay ) );
+
+    },
+
+    initialize : function() {
+
+      app.effects.apply( 'fade', 200 );
+
+      setTimeout( () => {
+
+        app.effects.apply( 'fade-from-bottom', 200 );
+
+      }, 400 );
+
+    }
+
+  },
+
+  load : function() {
+
+    setTimeout( () => {
+
+      app.element.dataset.status = 'loaded';
+
+      app.effects.initialize()
+
+    }, 600 )
+
+  },
+
+  initialize : function() {
+
+    window.addEventListener( 'load', app.load() );
+
   }
-  
+
 }
+
+app.initialize();
